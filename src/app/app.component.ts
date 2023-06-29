53fd518328b4f0c583faad72e4a8436a14b45b1e
unknown <artur.haiduk@zazmic.com>
2023-06-29 13:35:16 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'passwordVerification';
+  
+  password!: string;
 }
---
+++ src/app/password-strength-checker/password-strength-checker.component.ts
@@ -0,0 +1,35 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-password-strength-checker',
+  templateUrl: './password-strength-checker.component.html',
+  styleUrls: ['./password-strength-checker.component.scss']
+})
+export class PasswordStrengthCheckerComponent {
+  password: string = '';
+  strengthLevel: string = '';
+  strengthLabel: string = '';
+
+  calculateStrength(): void {
+    const passwordLength = this.password.length;
+    const hasLetters = /[a-zA-Z]/.test(this.password);
+    const hasNumbers = /[0-9]/.test(this.password);
+    const hasSymbols = /[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]/.test(this.password);
+
+    if (passwordLength === 0) {
+      this.strengthLevel = 'gray';
+    } else if (passwordLength < 8) {
+      this.strengthLevel = 'red';
+      this.strengthLabel = 'Add more characters';
+    } else if (hasLetters && hasNumbers && hasSymbols) {
+      this.strengthLevel = 'green';
+      this.strengthLabel = 'Stong';
+    } else if ((hasLetters && hasNumbers) || (hasLetters && hasSymbols)|| (hasNumbers && hasSymbols)) {
+      this.strengthLevel = 'yellow';
+      this.strengthLabel = 'Medium';
+    } else {
+        this.strengthLevel = 'simple';
+        this.strengthLabel = 'Simple';
+    }
+  }
+}
